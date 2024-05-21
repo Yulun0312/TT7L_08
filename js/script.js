e7e0933fab9b4502c99924d34043c57a9852febe
GanYuLun <1221108624@student.mmu.edu.my>
2024-05-21 16:35:57 +0800

    added line in html css and javascript

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navber');
+
+menu.onclick = () =>{
+    menu.clssliist.toggle('fa-times');
+    navbar.clssliist.toggle('active');
+}
+
+menu.onscroll = () =>{
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+
+    menu.clssliist.remove('fa-times');
+    navbar.clssliist.toggle('active');
+}
